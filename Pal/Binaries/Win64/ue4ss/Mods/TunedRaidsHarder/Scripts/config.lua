4157d9a72e11e7eda447eb64a5949cc72a27e7fb
Dhampyr <Dhampyru@gmail.com>
2025-07-20 14:19:11 -0500

    Raid Changes
    Made them less stupid

--- Pal/Binaries/Win64/ue4ss/Mods/TunedRaidsHarder/Scripts/config.lua
+++ Pal/Binaries/Win64/ue4ss/Mods/TunedRaidsHarder/Scripts/config.lua
@@ -13,16 +13,16 @@ local palConfig = {
     palRedColorWrapperBack = "\27[0m\n",
 
     -- Raid probability, default is 0.8
-	InvadeProbability = 1.0,
+	InvadeProbability = 0.8,
 	
 	-- Raids start from thos player's level, default is 5
 	InvadeOccurablePlayerLevel = 2,
 	
 	-- Raid party spawn this far from the base min, default is 10000
-	InvadeStartPoint_BaseCampRadius_Min_cm = 10000,
+	InvadeStartPoint_BaseCampRadius_Min_cm = 25000,
 	
 	-- Raid party spawn this far from the base max, default is 50000
-	InvadeStartPoint_BaseCampRadius_Max_cm = 25000
+	InvadeStartPoint_BaseCampRadius_Max_cm = 40000
 	
 	-- Minimum delay between raids, default is 30
 	--InvadeCollTime_Min_Minutes = 0,
