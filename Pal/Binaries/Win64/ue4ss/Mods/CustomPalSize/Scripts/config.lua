a542cef35ae3aa88e609b1667ac2df0ef341fd06
Dhampyr <Dhampyru@gmail.com>
2025-07-20 11:45:10 -0500

    Added pal resize mod
    Needs testing

---
+++ Pal/Binaries/Win64/ue4ss/Mods/CustomPalSize/Scripts/config.lua
@@ -0,0 +1,34 @@
+-- Author: yakuzadeso
+
+local config = {}
+
+--------------------------------------------------------------------------------
+-- Settings
+
+-- Rare pals
+config.affectsRarePalsOnly = false
+config.scaleRarePal = 0.3
+
+-- XL pals
+config.affectsXL = true
+config.scaleXL = 0.6
+
+-- L pals
+config.affectsL = true
+config.scaleL = 0.6
+
+-- M pals
+config.affectsM = true
+config.scaleM = 0.6
+
+-- S pals
+config.affectsS = true
+config.scaleS = 0.6
+
+-- XS pals
+config.affectsXS = true
+config.scaleXS = 0.6
+
+--------------------------------------------------------------------------------
+
+return config
