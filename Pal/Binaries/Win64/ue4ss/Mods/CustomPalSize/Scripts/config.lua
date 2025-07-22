3997e6ec940e2510cddd01efe92c050435f12cee
Dhampyr <Dhampyru@gmail.com>
2025-07-21 19:01:05 -0500

    Update config.lua
    Updates Pal Size for Testing

--- Pal/Binaries/Win64/ue4ss/Mods/CustomPalSize/Scripts/config.lua
+++ Pal/Binaries/Win64/ue4ss/Mods/CustomPalSize/Scripts/config.lua
@@ -4,18 +4,19 @@ local config = {}
 
 --------------------------------------------------------------------------------
 -- Settings
-
+-- (default in game is 1.0, default set in mod is 0.6)
+ 
 -- Rare pals
-config.affectsRarePalsOnly = true
+config.affectsRarePalsOnly = false
 config.scaleRarePal = 0.3
 
 -- XL pals
 config.affectsXL = true
-config.scaleXL = 0.6
+config.scaleXL = 0.7
 
 -- L pals
 config.affectsL = true
-config.scaleL = 0.6
+config.scaleL = 0.7
 
 -- M pals
 config.affectsM = true
@@ -27,7 +28,7 @@ config.scaleS = 0.6
 
 -- XS pals
 config.affectsXS = true
-config.scaleXS = 0.6
+config.scaleXS = 0.5
 
 --------------------------------------------------------------------------------
 
