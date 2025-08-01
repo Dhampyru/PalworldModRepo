af0a8759bc9d345ffda104ed34f13c0ea8a52543
Dhampyr <Dhampyru@gmail.com>
2025-08-01 00:01:36 -0500

    added back alternative pal summon

---
+++ Pal/Binaries/Win64/ue4ss/Mods/AlternativePalSummon/Scripts/config.lua
@@ -0,0 +1,7 @@
+local config = {}
+
+--- Same size as the default blue base ring, careful not to increase this too high or you'll send your pal to outer space
+--- Ideal maximum is 7200.0
+config.MaxSummonDistance = 3600.0
+
+return config
